dca8fd173b6ea089997291582f05cc37e994aae6
xaddriana <135568851+xaddriana@users.noreply.github.com>
2023-08-24 17:59:45 -0700

    Add files via upload

---
+++ SurfsUp/app.py
@@ -0,0 +1,44 @@
+# Import the dependencies.
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, 
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+#################################################
+# Flask Routes
+#################################################
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
